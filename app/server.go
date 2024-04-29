7f667b45534078423a1917d2bc067753cb87cc5d
sagoresarker <sagoresarker.cse@gmail.com>
2024-04-29 23:21:47 +0600

    pass stage

--- app/server.go
+++ app/server.go
@@ -1,175 +1,210 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
-	"io/ioutil"
+	"io"
+	"log"
 	"net"
 	"os"
-	"path/filepath"
+	path2 "path"
 	"strings"
 )
 
-type httpStatusCodes struct {
-	OK        []byte
-	NOT_FOUND []byte
-}
-
-var HttpStatus httpStatusCodes = httpStatusCodes{
-	OK:        []byte("HTTP/1.1 200 OK\r\n"),
-	NOT_FOUND: []byte("HTTP/1.1 404 Not Found\r\n\r\n"),
-}
-
-type httpMethods struct {
-	GET  string
-	POST string
-}
-
-var HttpMethod = httpMethods{
-	GET:  "GET",
-	POST: "POST",
-}
-
 type Request struct {
-	Method  string
-	Path    string
-	Headers map[string]string
+	Method    string
+	Path      string
+	Protocol  string
+	Host      string
+	UserAgent string
+	Body      string
 }
 
-func extractRequest(conn net.Conn) (*Request, error) {
-	msg := make([]byte, 1024)
-	reqLen, err := conn.Read(msg)
-	if err != nil {
-		return nil, err
-	}
+const (
+	CRLF             = "\r\n"
+	OK               = "HTTP/1.1 200 OK"
+	NotFound         = "HTTP/1.1 404 Not Found"
+	BadRequest       = "HTTP/1.1 400 Bad Request"
+	InternalError    = "HTTP/1.1 500 Internal Server Error"
+	ContentTypeText  = "Content-Type: text/plain"
+	ContentTypeOctet = "Content-Type: application/octet-stream"
+	ContentLength    = "Content-Length:"
+	Created          = "HTTP/1.1 201 Created"
+)
 
-	rawReq := string(msg[:reqLen])
-	lines := strings.Split(rawReq, "\r\n")
-	requestLine := strings.Split(lines[0], " ")
-	method := requestLine[0]
-	path := requestLine[1]
+var (
+	directory string
+)
 
-	headers := make(map[string]string)
-	for i := 1; i < len(lines)-2; i++ {
-		headerParts := strings.Split(lines[i], ": ")
-
-		if len(headerParts) == 2 {
-			headers[headerParts[0]] = headerParts[1]
-		}
-	}
+func newRequest(method string, path string, protocol string, host string, userAgent string, body string) *Request {
 	return &Request{
-		Method:  method,
-		Path:    path,
-		Headers: headers,
-	}, nil
+		Method:    method,
+		Path:      path,
+		Protocol:  protocol,
+		Host:      host,
+		UserAgent: userAgent,
+		Body:      body,
+	}
 }
 
-func handleConnection(conn net.Conn, directory string) {
+func handleConnection(conn net.Conn) {
 	defer conn.Close()
 
-	req, err := extractRequest(conn)
-	if err != nil {
-		fmt.Println("Error reading request: ", err.Error())
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
+	if err != nil && err != io.EOF {
+		log.Printf("Error reading: %v", err)
 		return
 	}
 
-	if strings.HasPrefix(req.Path, "/files/") {
-		filename := strings.TrimPrefix(req.Path, "/files/")
-		filePath := filepath.Join(directory, filename)
-		fileContents, err := ioutil.ReadFile(filePath)
-		if err != nil {
-			fmt.Println("Error reading file: ", err.Error())
-			_, err = conn.Write(HttpStatus.NOT_FOUND)
-			if err != nil {
-				fmt.Println("Error writing response: ", err.Error())
-			}
-			return
-		}
+	buf = bytes.Trim(buf, "\x00")
+	reqString := string(buf[:n])
+	reqStringSlice := strings.Split(reqString, CRLF)
 
-		contentLengthHeader := fmt.Sprintf("Content-Length: %d\r\n\r\n", len(fileContents))
-		response := append(HttpStatus.OK, []byte("Content-Type: application/octet-stream\r\n")...)
-		response = append(response, []byte(contentLengthHeader)...)
-		response = append(response, fileContents...)
+	if len(reqStringSlice) < 3 {
+		sendResponse(conn, BadRequest, "")
+		return
+	}
 
-		_, err = conn.Write(response)
-		if err != nil {
-			fmt.Println("Error writing response: ", err.Error())
-			return
-		}
-	} else if req.Path == "/user-agent" {
-		userAgent, ok := req.Headers["User-Agent"]
-		if !ok {
-			userAgent = ""
-		}
-		contentLengthHeader := fmt.Sprintf("Content-Length: %d\r\n\r\n", len(userAgent))
-		response := append(HttpStatus.OK, []byte("Content-Type: text/plain\r\n")...)
-		response = append(response, []byte(contentLengthHeader)...)
-		response = append(response, []byte(userAgent)...)
+	startLineSlice := strings.Split(reqStringSlice[0], " ")
+	if len(startLineSlice) < 3 {
+		log.Println("Invalid start line in request")
+		sendResponse(conn, BadRequest, "")
+		return
+	}
 
-		_, err = conn.Write(response)
-		if err != nil {
-			fmt.Println("Error writing response: ", err.Error())
-			return
-		}
-	} else if strings.HasPrefix(req.Path, "/echo/") {
-		echoContent := strings.TrimPrefix(req.Path, "/echo/")
-		contentLengthHeader := fmt.Sprintf("Content-Length: %d\r\n\r\n", len(echoContent))
-		response := append(HttpStatus.OK, []byte("Content-Type: text/plain\r\n")...)
-		response = append(response, []byte(contentLengthHeader)...)
-		response = append(response, []byte(echoContent)...)
+	headers := parseHeaders(reqStringSlice[1:])
+	host := headers["host"]
+	userAgent := headers["user-agent"]
+	body := reqStringSlice[len(reqStringSlice)-1]
 
-		_, err = conn.Write(response)
-		if err != nil {
-			fmt.Println("Error writing response: ", err.Error())
-			return
+	request := newRequest(startLineSlice[0], startLineSlice[1], startLineSlice[2], host, userAgent, body)
+
+	fmt.Printf("New Request: %s %s %s\n", request.Method, request.Path, request.Protocol)
+
+	switch {
+	case request.Path == "/":
+		sendResponse(conn, OK, "")
+	case strings.HasPrefix(request.Path, "/echo"):
+		handleEcho(conn, request)
+	case request.Path == "/user-agent":
+		handleUserAgent(conn, request)
+	case strings.HasPrefix(request.Path, "/files"):
+		if request.Method == "POST" {
+			handleUploadFile(conn, request)
+			break
 		}
-	} else {
-		switch req.Path {
-		case "/":
-			_, err = conn.Write(append(HttpStatus.OK, []byte("\r\n")...))
-			if err != nil {
-				fmt.Println("Error writing response: ", err.Error())
-				return
-			}
-		default:
-			_, err = conn.Write(HttpStatus.NOT_FOUND)
-			if err != nil {
-				fmt.Println("Error writing response: ", err.Error())
-				return
-			}
-		}
+		handleDownLoadFile(conn, request)
+	default:
+		sendResponse(conn, NotFound, "")
 	}
 }
 
-func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	directory := flag.String("directory", "", "The directory to serve files from")
-	flag.Parse()
-
-	if *directory == "" {
-		fmt.Println("Please provide a directory using the --directory flag")
+func parseHeaders(lines []string) map[string]string {
+	headers := make(map[string]string)
+	for _, line := range lines {
+		if line == "" {
+			break
+		}
+		parts := strings.SplitN(line, ": ", 2)
+		if len(parts) == 2 {
+			headers[strings.ToLower(parts[0])] = parts[1]
+		}
 	}
-	fmt.Printf("Home Directory: %v\n", *directory)
+	return headers
+}
 
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
+func main() {
+	listener, err := net.Listen("tcp", "0.0.0.0:4221")
+	flag.StringVar(&directory, "directory", "files", "path to the file directory")
+	flag.Parse()
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-
-	defer l.Close()
-
 	for {
-		conn, err := l.Accept()
+		conn, err := listener.Accept()
 		if err != nil {
-			fmt.Println("Error accepting connection: ", err.Error())
-			os.Exit(1)
+			log.Fatal("Failed to accept connection")
 		}
-
-		go handleConnection(conn, *directory)
-
+		go handleConnection(conn)
 	}
+}
 
+func handleEcho(conn net.Conn, request *Request) {
+	body, found := strings.CutPrefix(request.Path, "/echo/")
+	if !found {
+		fmt.Println("Failed to parse request")
+		handleServerError(conn)
+		return
+	}
+	sendResponse(conn, OK, body)
+}
+
+func handleDownLoadFile(conn net.Conn, request *Request) {
+	fileName, found := strings.CutPrefix(request.Path, "/files/")
+	if !found {
+		fmt.Println("Failed to parse request")
+		handleServerError(conn)
+		return
+	}
+	path := path2.Join(directory, fileName)
+	file, err := os.ReadFile(path)
+	if err != nil {
+		fmt.Println("Failed to open file \t" + path)
+		sendResponse(conn, NotFound, "")
+		return
+	}
+	downloadFile(conn, OK, file)
+}
+
+func handleUploadFile(conn net.Conn, request *Request) {
+	fileName, found := strings.CutPrefix(request.Path, "/files/")
+	if !found {
+		fmt.Println("Failed to parse request")
+		handleServerError(conn)
+		return
+	}
+	path := path2.Join(directory, fileName)
+	err := os.WriteFile(path, []byte(request.Body), 0644)
+	if err != nil {
+		handleServerError(conn)
+		return
+	}
+	saveFile(conn)
+}
+
+func handleUserAgent(conn net.Conn, request *Request) {
+	sendResponse(conn, OK, request.UserAgent)
+}
+
+func handleServerError(conn net.Conn) {
+	sendResponse(conn, InternalError+CRLF+CRLF, "")
+}
+
+func sendResponse(conn net.Conn, status string, body string) {
+	response := fmt.Sprintf("%s\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", status, len(body), body)
+	_, err := conn.Write([]byte(response))
+	if err != nil {
+		log.Printf("Error writing response: %v", err)
+		handleServerError(conn)
+	}
+}
+
+func downloadFile(conn net.Conn, status string, body []byte) {
+	response := fmt.Sprintf("%s\r\nContent-Type: application/octet-stream\r\nContent-Length: %d\r\n\r\n%s", status, len(body), body)
+	_, err := conn.Write([]byte(response))
+	if err != nil {
+		log.Printf("Error downloading file: %v", err)
+		handleServerError(conn)
+	}
+}
+
+func saveFile(conn net.Conn) {
+	_, err := conn.Write([]byte(Created + CRLF + CRLF))
+	if err != nil {
+		log.Printf("Error downloading file: %v", err)
+		handleServerError(conn)
+	}
 }
