a2836c3d0c949787d4c72ae5f2ee163c529ac9a4
sagoresarker <sagoresarker.cse@gmail.com>
2024-04-29 22:22:23 +0600

    pass stage

--- app/server.go
+++ app/server.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"fmt"
-	"strings"
-
-	// Uncomment this block to pass the first stage
 	"net"
 	"os"
+	"strings"
 )
 
 type httpStatusCodes struct {
@@ -15,7 +13,7 @@ type httpStatusCodes struct {
 }
 
 var HttpStatus httpStatusCodes = httpStatusCodes{
-	OK:        []byte("HTTP/1.1 200 OK\r\n\r\n"),
+	OK:        []byte("HTTP/1.1 200 OK\r\n"),
 	NOT_FOUND: []byte("HTTP/1.1 404 Not Found\r\n\r\n"),
 }
 
@@ -36,28 +34,19 @@ type Request struct {
 
 func extractRequest(conn net.Conn) (*Request, error) {
 	msg := make([]byte, 1024)
-
 	reqLen, err := conn.Read(msg)
-
 	if err != nil {
-
 		return nil, err
-
 	}
 
 	rawReq := string(msg[:reqLen])
-
 	lines := strings.Split(rawReq, "\n")
-
 	method := strings.Split(lines[0], " ")[0]
-
 	path := strings.Split(lines[0], " ")[1]
 
 	return &Request{
-
 		Method: method,
-
-		Path: path,
+		Path:   path,
 	}, nil
 }
 
@@ -65,8 +54,6 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
-	//
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
@@ -80,44 +67,39 @@ func main() {
 	}
 
 	req, err := extractRequest(conn)
-
 	if err != nil {
-
 		fmt.Println("Error reading request: ", err.Error())
-
 		os.Exit(1)
-
 	}
 
-	fmt.Println(req.Path)
+	if strings.HasPrefix(req.Path, "/echo/") {
+		echoContent := strings.TrimPrefix(req.Path, "/echo/")
+		contentLengthHeader := fmt.Sprintf("Content-Length: %d\r\n\r\n", len(echoContent))
+		response := append(HttpStatus.OK, []byte("Content-Type: text/plain\r\n")...)
+		response = append(response, []byte(contentLengthHeader)...)
+		response = append(response, []byte(echoContent)...)
 
-	switch req.Path {
-
-	case "/":
-
-		fmt.Println("case /")
-
-		_, err = conn.Write(HttpStatus.OK)
+		_, err = conn.Write(response)
 		if err != nil {
-
 			fmt.Println("Error writing response: ", err.Error())
-
 			os.Exit(1)
-
 		}
-
-	default:
-
-		fmt.Println("case 404")
-
-		_, err = conn.Write(HttpStatus.NOT_FOUND)
-		if err != nil {
-
-			fmt.Println("Error writing response: ", err.Error())
-
-			os.Exit(1)
-
+	} else {
+		switch req.Path {
+		case "/":
+			fmt.Println("case /")
+			_, err = conn.Write(append(HttpStatus.OK, []byte("\r\n")...))
+			if err != nil {
+				fmt.Println("Error writing response: ", err.Error())
+				os.Exit(1)
+			}
+		default:
+			fmt.Println("case 404")
+			_, err = conn.Write(HttpStatus.NOT_FOUND)
+			if err != nil {
+				fmt.Println("Error writing response: ", err.Error())
+				os.Exit(1)
+			}
 		}
-
 	}
 }
